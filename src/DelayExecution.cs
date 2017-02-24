092b585428306d5bf763307ebcbf6dd33e286bc0
Alex Davies <alex@codecutout.com>
2017-02-24 00:08:06 +0000

    Initial Commit.

---
+++ src/DelayExecution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rerunner
+{
+    public class DelayExecution
+    {
+        private readonly int _delayTime;
+        private readonly Timer _timer;
+        public DelayExecution(Action action, int delayTime)
+        {
+            _delayTime = delayTime;
+            _timer = new Timer(new TimerCallback(_ =>
+            {
+                action();
+            }));
+        }
+
+        public void Start()
+        {
+            _timer.Change(_delayTime, Timeout.Infinite);
+        }
+    }
+}
---
+++ test/SampleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("merry xmas");
+            using (WebApp.Start<Startup>("http://localhost:8087"))
+            {
+                Console.WriteLine("Web Server is running..");
+                Console.WriteLine("Press any key to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup{
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure Web API for self-host. 
+            //var config = new HttpConfiguration();
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world 11.");
+            });
+        }
+    }
+}
